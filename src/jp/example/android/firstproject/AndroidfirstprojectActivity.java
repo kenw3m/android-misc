8f0a7931e5c61a990119602365beababe7dd86e8
Default@ken200 <Default@ken200-5b0473c3>
2011-09-03 13:21:14 +0900

    Set wallpaper

--- src/jp/example/android/firstproject/AndroidfirstprojectActivity.java
+++ src/jp/example/android/firstproject/AndroidfirstprojectActivity.java
@@ -1,5 +1,6 @@
 package jp.example.android.firstproject;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,6 +8,7 @@ import java.net.URL;
 
 import android.app.Activity;
 import android.app.Application;
+import android.app.WallpaperManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -31,15 +33,23 @@ public class AndroidfirstprojectActivity extends Activity {
 					URL url = new URL("http://yusa.imouto.org/sample/413a5a793d06d4aae93fbca4c51965ca/moe%20192233%20sample.jpg");
 				    InputStream urlst = url.openStream();
 				    FileOutputStream fo = openFileOutput("file.jpg", MODE_PRIVATE);
-				    int r;
-				    while (0 <= (r = urlst.read())){
-				    	fo.write(r);
-				    }
+//				    int r;
+//				    while (0 <= (r = urlst.read())){
+//				    	fo.write(r);
+//				    }
+//				    urlst.reset();
+				    WallpaperManager walman = WallpaperManager.getInstance(getApplicationContext());
+				    //walman.suggestDesiredDimensions(640, 640);
+				    walman.setStream(urlst);
+				    
 				    fo.flush();
 				    fo.close();
 				    urlst.close();
-				} catch (IOException e){
+				    
+				} catch (Exception e){
 					Log.d(getString(R.string.app_name), e.getMessage());
+					File f = new File("file.jpg");
+					f.delete();
 				}
 				Toast.makeText(getApplicationContext(), "Downloaded!!", Toast.LENGTH_LONG).show();				    
 				
