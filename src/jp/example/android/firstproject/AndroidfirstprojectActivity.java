395ea61d4fe9afd400c8da7b231771247db21325
Default@ken200 <Default@ken200-5b0473c3>
2011-09-04 15:06:21 +0900

    巨大BMP対策

--- src/jp/example/android/firstproject/AndroidfirstprojectActivity.java
+++ src/jp/example/android/firstproject/AndroidfirstprojectActivity.java
@@ -9,8 +9,16 @@ import java.net.URL;
 import android.app.Activity;
 import android.app.Application;
 import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapFactory.Options;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
+import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -29,32 +37,86 @@ public class AndroidfirstprojectActivity extends Activity {
 				// TODO Auto-generated method stub
 				Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_LONG).show();
 				
-				try {
-					URL url = new URL("http://yusa.imouto.org/sample/413a5a793d06d4aae93fbca4c51965ca/moe%20192233%20sample.jpg");
-				    InputStream urlst = url.openStream();
-				    FileOutputStream fo = openFileOutput("file.jpg", MODE_PRIVATE);
-//				    int r;
-//				    while (0 <= (r = urlst.read())){
-//				    	fo.write(r);
-//				    }
-//				    urlst.reset();
-				    WallpaperManager walman = WallpaperManager.getInstance(getApplicationContext());
-				    //walman.suggestDesiredDimensions(640, 640);
-				    walman.setStream(urlst);
-				    
-				    fo.flush();
-				    fo.close();
-				    urlst.close();
-				    
-				} catch (Exception e){
-					Log.d(getString(R.string.app_name), e.getMessage());
-					File f = new File("file.jpg");
-					f.delete();
-				}
-				Toast.makeText(getApplicationContext(), "Downloaded!!", Toast.LENGTH_LONG).show();				    
-				
+			    BitmapFactory.Options opts = new Options();
+			    opts.inPurgeable = true;
+			    opts.inSampleSize = 3;
+			    
+			    Bitmap obmp = BitmapFactory.decodeResource(getResources(), R.drawable.large, opts);
+			    WallpaperManager walman = WallpaperManager.getInstance(getApplicationContext());
+			    //walman.suggestDesiredDimensions(640, 640);
+			    try{
+			    walman.setBitmap(obmp);
+			    }catch(IOException e){
+			    	Log.d("test", e.getMessage());
+			    }
+//				new GetImageFileTask().execute("http://yusa.imouto.org/image/b92803b1959999ec2ef4cfa3de80fa99/moe%20192448%20mitha%20seifuku%20thighhighs.jpg","http://yusa.imouto.org/sample/df736b7cadb485bfeafbfc113a53e07c/moe%20192104%20sample.jpg");
+//				
+//				try {
+//					URL url = new URL("http://kyamon.com/wp-content/uploads/2011/07/google_logo.jpg");
+//				    InputStream urlst = url.openStream();
+//				    FileOutputStream fo = openFileOutput("file.jpg", MODE_PRIVATE);
+////				    int r;
+////				    while (0 <= (r = urlst.read())){
+////				    	fo.write(r);
+////				    }
+////				    urlst.reset();
+//				    WallpaperManager walman = WallpaperManager.getInstance(getApplicationContext());
+//				    //walman.suggestDesiredDimensions(640, 640);
+//				    walman.setStream(urlst);
+//				    
+//				    fo.flush();
+//				    fo.close();
+//				    urlst.close();
+//				    
+//				} catch (Exception e){
+//					Log.d(getString(R.string.app_name), e.getMessage());
+//					File f = new File("file.jpg");
+//					f.delete();
+//				}
+//				
 			}
 		});
         
+    }
+    
+    class GetImageFileTask extends AsyncTask<String, Void, Boolean>{
+    	private Exception exception;
+
+    	@Override
+    	protected Boolean doInBackground(String... vurl) {
+    		try{
+    			URL url = new URL(vurl[0]);
+    			InputStream is = url.openStream();
+			    WallpaperManager walman = WallpaperManager.getInstance(getApplicationContext());	  
+//			    walman.setStream(is);
+
+			    DisplayMetrics metric = new DisplayMetrics();
+			    ((WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(metric);
+			    int dsize = Math.max(metric.heightPixels, metric.widthPixels);			   
+			    float scale = 1.0F;	
+			    BitmapFactory.Options opts = new Options();
+			    opts.inPurgeable = true;
+			    
+			    Bitmap obmp = BitmapFactory.decodeStream(is,null,opts);
+			    scale = dsize / (float)Math.min(obmp.getWidth(), obmp.getHeight());
+			    obmp = Bitmap.createScaledBitmap(obmp, (int)(scale*obmp.getWidth()), (int)(scale*obmp.getHeight()), true);
+			    walman.setBitmap(obmp);
+
+    		} catch(IOException e){
+    			this.exception = e;
+    			Log.d(getString(R.string.app_name), e.getMessage());
+    			return false;
+    		}
+    		
+    		return true;
+		}
+    	protected void onPostExecute(Boolean finished){
+    		if (finished){
+    			Toast.makeText(getApplicationContext(), "Downloaded!!", Toast.LENGTH_LONG).show();
+    		}else{
+    			Toast.makeText(getApplicationContext(), "Download failed", Toast.LENGTH_LONG).show();
+    		}
+    		return;
+    	}
     }
 }
